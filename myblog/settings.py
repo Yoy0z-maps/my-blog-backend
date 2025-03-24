2445cdab5aa98dcbae9a71e1c6c9356f4674cced
John Han <work.johnhan@gmail.com>
2025-03-24 21:41:58 +0800

    Feat: 프론트 개발 서버 API 테스트 CORS 설정

--- myblog/settings.py
+++ myblog/settings.py
@@ -61,10 +61,12 @@ INSTALLED_APPS = [
     'users',
     'posts',
     'django_filters',
-    'projects'
+    'projects',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,6 +74,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
 
 ROOT_URLCONF = 'myblog.urls'
