bda7230ca4c78debb10018af4d96e195a1174253
John Han <work.johnhan@gmail.com>
2025-03-24 15:13:13 +0800

    settings.py => collectstatic setting

--- myblog/settings.py
+++ myblog/settings.py
@@ -17,6 +17,7 @@ from decouple import config, Csv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
