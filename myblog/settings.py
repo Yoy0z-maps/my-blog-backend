34298ac9e5b4192693f300b4f4f5c598ae28f558
John Han <work.johnhan@gmail.com>
2025-04-01 17:52:33 +0800

    Chore: 패키지 설치 및 설정

--- myblog/settings.py
+++ myblog/settings.py
@@ -63,7 +63,16 @@ INSTALLED_APPS = [
     'django_filters',
     'projects',
     'corsheaders',
+    'storages',
 ]
+
+# AWS S3 Config
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_STORAGE_BUCKET_NAME = 'my-blog-yoy0z-maps-bucket'
+AWS_S3_REGION_NAME = 'ap-southeast-2'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
