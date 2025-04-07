6151c51e9f216ca5ded02890074bc709761ae810
John Han <work.johnhan@gmail.com>
2025-04-07 21:29:59 +0800

    remove cached __pycache__ files

--- posts/models.py
+++ posts/models.py
@@ -18,3 +18,9 @@ class Post(models.Model):
     likes = models.IntegerField(default=0)
     published_date = models.DateTimeField(default=timezone.now)
     category = models.CharField(max_length=128, default='default')
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nickname = models.CharField(max_length=128)
+    text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
